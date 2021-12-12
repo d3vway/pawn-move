334dd13fac123293d80d0efc4fcbb5fbe061d53d
Yusuf T420 <mhyusufibrahim@gmail.com>
2021-12-12 19:12:43 +0700

    Include hints, first move should be able to steps

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-const rank = ["A", "B", "C", "D", "E", "F", "G", "H"];
-const file = [1, 2, 3, 4, 5, 6, 7, 8];
+const rank = [1, 2, 3, 4, 5, 6, 7, 8];
+const file = ["A", "B", "C", "D", "E", "F", "G", "H"];
 let currentTile = {};
 let count = 0;
-let tiles = $(".tile");
 
-const initBoard = function () {
-    for (let r = 0; r < rank.length; r++) {
+const init = function () {
+    for (let r = rank.length; r >  0; r--) {
         for (let f = 0; f < file.length; f++) {
 
+            // Append tile
+            let style = '';
             if (count % 8 == 0) {
-                $("#chess-board").append('<div class="tile" style="clear:left"></div>');
-
-            } else {
-                $("#chess-board").append('<div class="tile"></div>');
+                style = 'clear:left';
             }
 
-            $(".tile").eq(count).attr("tile-pos", ((rank[rank.length - (r + 1)] + file[f])));
+            let coordinate = file[f] + r;
+            $("#chess-board").append('<div class="tile" style="' + style + '">' + coordinate + '</div>');
 
+            $(".tile").eq(count).attr("coordinate", coordinate);
+
+            // Tile's color
             if (((r % 2 == 0) && (f % 2 != 0)) || ((r % 2 != 0) && (f % 2 == 0))) {
                 $(".tile").eq(count).addClass("black");
             } else {
@@ -27,28 +29,42 @@ const initBoard = function () {
     }
 }
 
+let moveCount = 0;
 const legalMove = function () {
-    tiles.removeClass('legal');
-    let gridpos = currentTile.attr('tile-pos');
-    let l_rank = ($.inArray(gridpos.charAt(0), rank));
-    let l_file = ($.inArray(parseInt(gridpos.charAt(1)), file));
-    let ID = rank[l_rank + 1] + file[l_file];
-    $('*[tile-pos="' + ID + '"]').addClass('legal');
+    $(".tile").removeClass('legal');
+
+    const x = currentTile.attr('coordinate');
+    const f = x.charAt(0);
+    const r = parseInt(x.charAt(1));
+
+    if (moveCount <= 1) {
+        for (let step=r;step<=(r+2);step++) {
+            $('*[coordinate="' + f + (step) + '"]').addClass('legal');
+        }
+    } else {
+        $('*[coordinate="' + f+(r+1) + '"]').addClass('legal');
+    }
+
     currentTile.removeClass('legal');
 
 }
 
-$( document ).ready(function($) {
-    initBoard();
-    let tiles = $(".tile");
+function placePawn(t) {
+    currentTile = $(t);
+    let midY = currentTile.position().top += (currentTile.width() / 2);
+    let midX = currentTile.position().left += (currentTile.width() / 2);
+    let player = $("#player");
+    $(".tile").removeClass('legal');
+    player.css({"top": midY - (0.5 * player.width()), "left": midX - (0.5 * player.width())});
+
+    moveCount++;
+}
+
+$(document).ready(function ($) {
+    init();
 
     $(".tile").on('click', function () {
-        currentTile = $(this);
-        let midY = $(this).position().top += ($(this).width() / 2);
-        let midX = $(this).position().left += ($(this).width() / 2);
-        let player = $("#player");
-        tiles.removeClass('legal');
-        player.css({"top": midY - (0.5 * player.width()), "left": midX - (0.5 * player.width())});
+        placePawn(this);
     })
         .on('mouseenter', function () {
             $(this).addClass('hover');
@@ -74,5 +90,6 @@ $( document ).ready(function($) {
         }
     })
 
-    $('*[tile-pos="A1"]').trigger('click');
+    $('*[coordinate="A1"]').trigger('click'); // first position
+
 });
